1457afe9d8cbf538dd1ff039b46fbed1e6ec2b55
Mazo Vasiliy <mazo@protonmail.ch>
2023-07-12 00:36:47 +0300

    nnn option changed, bat pager specified

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ set PATH $HOME/.local/bin $HOME/.cargo/bin $PATH
 
 setenv EDITOR nvim
 setenv NNN_PLUG "f:finder;c:fzcd;p:preview-tui;z:autojump"
-setenv NNN_OPTS "aACdu"
+setenv NNN_OPTS "aAcCdu"
 
 set host_config (status dirname)"$fold_config/config.fish#"(hostname | sed 's/\..*//')
 
