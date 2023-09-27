e267a7fd084643af65826ba41f112e0714d27f6b
Mazo Vasiliy <mazo@protonmail.ch>
2023-09-27 19:10:54 +0300

    jq colors

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,8 @@ setenv EDITOR nvim
 setenv NNN_PLUG "f:finder;c:fzcd;p:preview-tui;z:autojump"
 setenv NNN_OPTS "aAcCdu"
 
+setenv JQ_COLORS "0;39:0;39:0;39:0;39:0;32:1;39:1;39"
+
 set host_config (status dirname)"$fold_config/config.fish#"(hostname | sed 's/\..*//')
 
 if test -e "$host_config"
