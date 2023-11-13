9d5c7d122d1568281916a46b2643a076b99e9b03
Mazo Vasiliy <mazo@protonmail.ch>
2023-11-13 17:52:41 +0300

    layout indicator as standalone shell script; switched to dzen2

---
+++ scripts/.local/bin/layout.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# screen width
+sw=$(xwininfo -root | grep Width | cut -d ':' -f 2)
+# icon width
+iw=28
+# font
+font="Terminus:size=16:style=bold"
+# background
+bg="#432d59"
+# foreground
+fg="#00ff00"
+
+sh -c '
+    xkb-switch | tr a-z A-Z
+    while true; do 
+        xkb-switch --wait -p | tr a-z A-Z
+    done' | dzen2 -p 0 -bg "$bg" -fg "$fg" -fn "$font" -x $((sw-iw)) -w $iw -h $iw
+
