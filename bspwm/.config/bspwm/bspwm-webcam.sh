f24f6958313397d5c89360960a8b4748f347b25a
Mazo Vasiliy <mazo@protonmail.ch>
2023-12-24 02:42:49 +0300

    webcam script for bspwm

--- bspwm/.config/bspwm/bspwm-webcam.sh
+++ bspwm/.config/bspwm/bspwm-webcam.sh
@@ -1,24 +1,41 @@
 #!/bin/sh
 
-# screen width
-sw=$(xwininfo -root | grep Width  | cut -d ':' -f 2)
-sh=$(xwininfo -root | grep Height | cut -d ':' -f 2)
+TITLE=webcam
 
-# webcam width
-w=320
-h=240
+window_id=$(bspc query -N          \
+    | xargs xtitle -f "%u %s\n"    \
+    | grep -E '^[0-9]+[ ]+webcam$' \
+    | sed -E 's/[ ]+.*//')
 
-# gaps
-gapx=6
-gapy=6
+if test -n "$window_id"; then
+    bspc node "$window_id" --flag hidden
+else
+    # screen width
+    sw=$(xwininfo -root | grep Width  | cut -d ':' -f 2)
+    sh=$(xwininfo -root | grep Height | cut -d ':' -f 2)
 
-x=$((sw-w-gapx))
-y=$((sh-h-gapy))
+    # webcam width
+    w=320
+    h=240
 
-bspc rule -a '*' -o                 \
-    state=floating                  \
-    sticky=on                       \
-    center=off                      \
-    layer=above                     \
-    rectangle="${w}x${h}+${x}+${y}" \
-    && exec ffplay -f v4l2 -video_size "${w}x${h}" -framerate 25 -i /dev/video0 -fflags nobuffer
+    # gaps
+    gapx=6
+    gapy=6
+
+    x=$((sw-w-gapx))
+    y=$((sh-h-gapy))
+
+    bspc rule -a 'ffplay' -o            \
+        state=floating                  \
+        sticky=on                       \
+        center=off                      \
+        layer=above                     \
+        rectangle="${w}x${h}+${x}+${y}" \
+    && exec ffplay                      \
+        -f v4l2                         \
+        -window_title "$TITLE"          \
+        -video_size "${w}x${h}"         \
+        -framerate 25                   \
+        -i /dev/video0                  \
+        -fflags nobuffer
+fi
