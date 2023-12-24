b12d37625625e60b38b5b6cdf7a3424527513f5b
Mazo Vasiliy <mazo@protonmail.ch>
2023-12-24 20:07:15 +0300

    volume script refactored

--- bspwm/.config/bspwm/bspwm-volume.sh
+++ bspwm/.config/bspwm/bspwm-volume.sh
@@ -4,39 +4,40 @@ test -n "$1" || exit 0
 
 action="$1"
 
+sink=$(pactl list short sinks                 \
+    | grep RUNNING                            \
+    | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' \
+    | head -n 1)
+
+
 if test "$action" = "up"; then
 
     # увеличить громкость
-    pactl set-sink-volume $( \
-        pactl list short sinks \
-            | grep RUNNING \
-            | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' \
-            | head -n 1
-    ) +5%
+    pactl set-sink-mute $sink 0
+    pactl set-sink-volume $sink +5%
 
 elif test "$action" = "down"; then
 
     # уменьшить громкость
-    pactl set-sink-volume $( \
-        pactl list short sinks \
-            | grep RUNNING \
-            | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' \
-            | head -n 1 \
-    ) -5%
+    pactl set-sink-mute $sink 0
+    pactl set-sink-volume $sink -5%
 
 elif test "$action" = "mute"; then
 
     # замьютить
-    pactl set-sink-mute $( \
-        pactl list short sinks \
-            | grep RUNNING \
-            | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' \
-            | head -n 1 \
-    ) $( \
-        LC_ALL=C pactl list sinks \
-            | grep Mute \
-            | head -n 1 \
-            | cut -d ' ' -f 2 \
-            | sed 's/yes/0/;s/no/1/' \
-    )
+    mute_state=$(pactl get-sink-mute $sink \
+        | head -n 1                        \
+        | cut -d ' ' -f 2                  \
+        | sed 's/yes/0/;s/no/1/')
+
+    pactl set-sink-mute $sink $mute_state
 fi
+
+if test $(pactl get-sink-mute $sink | cut -d ':' -f 2 | tr -d ' ') = "yes"; then
+    message="Volume: mute"
+else
+    message="Volume: $( \
+        pactl get-sink-volume 1 | head -n 1 | cut -d '/' -f 2 | tr -d ' ')"
+fi
+
+./bspwm-message.sh "$message"
