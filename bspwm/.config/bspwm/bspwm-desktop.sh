88a1d7ca5defcebd53c712b773952f050b76e759
Mazo Vasiliy <mazo@protonmail.ch>
2023-12-24 20:08:28 +0300

    desktop script refactored

--- bspwm/.config/bspwm/bspwm-desktop.sh
+++ bspwm/.config/bspwm/bspwm-desktop.sh
@@ -19,20 +19,12 @@ mon_n=$(bspc query -M | sed -n "/$(bspc query -M -m focused)/{=;q;}")
 # номер десктопа в терминах bspwm
 desktop_n=$(((mon_n-1)*total_n+target_n))
 
-font="*-terminus-medium-*-*-*-32-*"
-
-if command -v "textwidth" >/dev/null; then
-    textwidth="textwidth"
-elif command -v "dzen2-textwidth" >/dev/null; then
-    textwidth="dzen2-textwidth"
-fi
-
-width=$(echo "$message" | "$textwidth" "$font" "$message")
-
 if test "$action" = "focus"; then
-    bspc desktop -f "$desktop_n" && \
-        echo "Workspace: $(bspc query -D -d focused --names)" \
-        | osd_cat -d 1 -w --pos middle --align center -c green --outline 2 -f "$font" -
+
+    bspc desktop -f "$desktop_n"
+
+    ./bspwm-message.sh "Workspace: $(bspc query -D -d focused --names)"
+
 elif test "$action" = "send"; then
     bspc node -d "$desktop_n"
 fi
