27318f5f6dd4e21b3fdf10f2b93f918e78a44c75
Mazo Vasiliy <mazo@protonmail.ch>
2024-01-02 02:43:34 +0300

    jqplay more size for preview

--- fish/.config/fish/functions/jqplay.fish
+++ fish/.config/fish/functions/jqplay.fish
@@ -2,18 +2,12 @@ function jqplay -d "Run jq REPL"
     if test -f $argv[1]
         set filename $argv[1]
 
-        if test -n $argv[2]
-            set query $argv[2]
-        else
-            set query "."
-        end
-
         jq "." "$filename" \
         | fzf \
             --disabled \
             --print-query \
             --preview "jq -C {q} \"$filename\"" \
-            --query="$query" \
+            --preview-window "up:99%" \
             --bind "up:preview-up,down:preview-down,page-up:preview-page-up,page-down:preview-page-down,home:preview-top,end:preview-bottom" \
             --no-sort \
             --tac \
