8614927db1dd4607507a7a746f327712b8473969
Mazo Vasiliy <mazo@protonmail.ch>
2024-01-04 23:19:00 +0300

    Fixed unintended behaviour when no windows found.

--- bspwm/.config/bspwm/bspwm-showhide.sh
+++ bspwm/.config/bspwm/bspwm-showhide.sh
@@ -4,8 +4,7 @@ test -n "$1" || exit 0
 
 class="$1"
 
-bspc node $( \
-    bspc query -N \
-    | xargs -I{} bspc query -T -n {} \
-    | jq "select(.client.className == \"$class\") | .id" \
-) --flag hidden
+bspc query -N                                        \
+| xargs -I{} bspc query -T -n {}                     \
+| jq "select(.client.className == \"$class\") | .id" \
+| xargs -I{} bspc node {} --flag hidden
