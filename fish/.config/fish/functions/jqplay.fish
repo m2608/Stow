ff291fc50542cb4449ac9614bf3316614e18d81f
Mazo Vasiliy <mazo@protonmail.ch>
2024-03-26 21:48:40 +0300

    jqplay upgraded

--- fish/.config/fish/functions/jqplay.fish
+++ fish/.config/fish/functions/jqplay.fish
@@ -1,16 +1,24 @@
 function jqplay -d "Run jq REPL"
-    if test -f $argv[1]
-        set filename $argv[1]
+    if test -f "$argv[1]"
+        set filename "$argv[1]"
+        set query "$argv[2]"
 
-        jq "." "$filename" \
-        | fzf \
-            --disabled \
-            --print-query \
-            --preview "jq -C {q} \"$filename\"" \
-            --preview-window "up:99%" \
-            --bind "up:preview-up,down:preview-down,page-up:preview-page-up,page-down:preview-page-down,home:preview-top,end:preview-bottom" \
-            --no-sort \
-            --tac \
+        cat "$filename"                                       \
+        | fzf                                                 \
+            --disabled                                        \
+            --print-query                                     \
+            --query "$query"                                  \
+            --preview "jq -C {q} '$filename'"                 \
+            --preview-window "up:99%"                         \
+            --bind "up:preview-up"                            \
+            --bind "down:preview-down"                        \
+            --bind "page-up:preview-page-up"                  \
+            --bind "page-down:preview-page-down"              \
+            --bind "home:preview-top"                         \
+            --bind "end:preview-bottom"                       \
+            --bind "alt-e:become(jq {q} '$filename' | jless)" \
+            --no-sort                                         \
+            --tac                                             \
         | head -1
     end
 end
