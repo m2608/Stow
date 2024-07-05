d20900ef58f182dd66e3ef38e1883f9ab1a0060e
Mazo Vasiliy <mazo@protonmail.ch>
2024-07-05 19:30:15 +0300

    disable colors for nnn

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,14 +3,14 @@ fish_add_path $HOME/.local/bin $HOME/.cargo/bin
 setenv EDITOR nvim
 setenv NVIM_GTK_NO_HEADERBAR 1
 setenv NNN_PLUG ";:fzplug;z:autojump;p:preview-tui;t:preview-tabbed"
-setenv NNN_OPTS "aAcCdu"
+setenv NNN_OPTS "aAcdu"
 
 setenv JQ_COLORS "0;39:0;39:0;39:0;39:0;32:1;39:1;39"
 
 bind \cz "jobs > /dev/null && fg"
 
 alias fc-select "fc-list | sed -r 's/^[^:]+:[ ]+([^,:]+)([,:].*)?/\1/' | sort -u | fzf --layout reverse-list"
-alias nnn "LANG=ru_RU.UTF-8 PAGER='nvimpager -p' "(which nnn)
+alias nnn "LANG=ru_RU.UTF-8 PAGER='nvimpager -p' NO_COLOR=1 "(which nnn)
 alias oil "nvim -c 'Oil'"
 
 set host_config (status dirname)"$fold_config/config.fish#"(hostname | sed 's/\..*//')
--- fish/.config/fish/functions/n.fish
+++ fish/.config/fish/functions/n.fish
@@ -28,6 +28,8 @@ function n --wraps nnn --description 'support nnn quit and change directory'
     # The command function allows one to alias this function to `nnn` without
     # making an infinitely recursive alias
     set -x LANG ru_RU.UTF-8 
+    set -x NO_COLOR 1
+    set -x PAGER 'nvimpager -p'
     command nnn $argv
 
     if test -e $NNN_TMPFILE
