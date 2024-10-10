84899abe872d5ee58e32caf4eed4afe52c0a7c57
Mazo Vasiliy <mazo@protonmail.ch>
2024-10-10 22:51:27 +0300

    jqplay hotkeys to view query and result

--- fish/.config/fish/functions/jqplay.fish
+++ fish/.config/fish/functions/jqplay.fish
@@ -3,22 +3,25 @@ function jqplay -d "Run jq REPL"
         set filename "$argv[1]"
         set query "$argv[2]"
 
-        cat "$filename"                                       \
-        | fzf                                                 \
-            --disabled                                        \
-            --print-query                                     \
-            --query "$query"                                  \
-            --preview "jq -C {q} '$filename'"                 \
-            --preview-window "up:99%"                         \
-            --bind "up:preview-up"                            \
-            --bind "down:preview-down"                        \
-            --bind "page-up:preview-page-up"                  \
-            --bind "page-down:preview-page-down"              \
-            --bind "home:preview-top"                         \
-            --bind "end:preview-bottom"                       \
-            --bind "alt-e:become(jq {q} '$filename' | jless)" \
-            --no-sort                                         \
-            --tac                                             \
+        cat "$filename"                                                        \
+        | fzf                                                                  \
+            --disabled                                                         \
+            --print-query                                                      \
+            --query "$query"                                                   \
+            --preview "jq -C {q} '$filename'"                                  \
+            --preview-window "up:99%"                                          \
+            --bind "up:preview-up"                                             \
+            --bind "down:preview-down"                                         \
+            --bind "page-up:preview-page-up"                                   \
+            --bind "page-down:preview-page-down"                               \
+            --bind "home:preview-top"                                          \
+            --bind "end:preview-bottom"                                        \
+            --bind "alt-e:execute(jq {q} '$filename' | nvim -c 'set ft=json')" \
+            --bind "alt-q:execute(echo {q} | nvim -c 'set ft=jq')"             \
+            --no-sort                                                          \
+            --tac                                                              \
         | head -1
     end
 end
+
+
