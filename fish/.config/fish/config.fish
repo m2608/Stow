6a8c7dcc725082cbd223d3d2404fc0b96e1518b8
Mazo Vasiliy <mazo@protonmail.ch>
2024-11-26 12:02:05 +0300

    better aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,10 +8,14 @@ setenv NNN_FIFO "/tmp/nnn.fifo"
 
 setenv JQ_COLORS "0;39:0;39:0;39:0;39:0;32:1;39:1;39"
 
-bind \cz "jobs > /dev/null && fg"
+bind \cz "jobs > /dev/null && echo && fg"
 
 alias fc-select "fc-list | sed -r 's/^[^:]+:[ ]+([^,:]+)([,:].*)?/\1/' | sort -u | fzf --layout reverse-list"
-alias nnn "LANG=ru_RU.UTF-8 PAGER='nvimpager -p' NO_COLOR=1 "(which nnn)
+
+set nnn (which nnn)
+if test -n "$nnn"
+    alias nnn "LANG=ru_RU.UTF-8 PAGER='nvimpager -p' NO_COLOR=1 $nnn"
+end
 alias oil "nvim -c 'Oil'"
 
 set host_config (status dirname)"$fold_config/config.fish#"(hostname | sed 's/\..*//')
