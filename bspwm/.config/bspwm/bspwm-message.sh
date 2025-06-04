5dce5f55f5a7941d29695198d5c69aa1e25820f1
Mazo Vasiliy <mazo@protonmail.ch>
2025-06-04 11:46:00 +0300

    better message size calculation

--- bspwm/.config/bspwm/bspwm-message.sh
+++ bspwm/.config/bspwm/bspwm-message.sh
@@ -9,25 +9,35 @@ get_resource() {
 
 message="$1"
 
+# get settings from resources
 font=$(get_resource "bspwm-message[.]font")
 bg=$(get_resource "bspwm-message[.]background")
 fg=$(get_resource "bspwm-message[.]foreground")
 
+# default settings
 : "${font:='Terminus:size=16'}"
 : "${bg:='#432d59'}"
 : "${fg:='#00ff00'}"
 
+# get font file and size for magick
+fontfile=$(fc-match -f "%{file}" "$font")
+fontsize=$(fc-match -f "%{size}" "$font")
+
+# get screen dpi
+dpi=$(xdpyinfo | sed -n -r '/^[ ]*resolution:[ ]+[0-9]+x[0-9]+ dots/ s/^[ ]*resolution:[ ]+([0-9]+)x([0-9]+) dots .*/(\1+\2)\/2/p' | bc)
 
 # message size
-mw=$(xftwidth "$font" "$message")
-mh=32
+size=$(magick -density $dpi -font "$fontfile" -pointsize $fontsize label:"$message" -format "%wx%h" info:)
+
+mw=$(echo $size | cut -d 'x' -f 1)
+mh=$(echo $size | cut -d 'x' -f 2)
 
 # screen size
 wininfo=$(xwininfo -root)
 sw=$(echo "$wininfo" | grep Width  | cut -d ':' -f 2)
 sh=$(echo "$wininfo" | grep Height | cut -d ':' -f 2)
 
-echo "$message" \
+printf "%s\n" "$message" \
 | dzen2 -p 1 -bg "$bg" -fg "$fg" -fn "$font" \
     -w $mw            \
     -h $mh            \
