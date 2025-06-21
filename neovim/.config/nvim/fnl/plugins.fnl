0afb7cdeef3bbd32eab45765543465a620ed064e
Mazo Vasiliy <mazo@protonmail.ch>
2025-06-21 14:13:13 +0300

    required part of nfnl config

--- neovim/.config/nvim/fnl/plugins.fnl
+++
@@ -1,5 +0,0 @@
-[{1 "folke/lazy.nvim"
-  :version "*"}
- {1 "Olical/nfnl"
-  :ft "fennel"
-  :init (fn [] (require "config"))}]
---
+++ neovim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/lazy.nvim",
+    version = "*"
+  },
+  {
+    "Olical/nfnl",
+    ft = "fennel",
+    init = function()
+      require("config")
+    end
+  }
+}
