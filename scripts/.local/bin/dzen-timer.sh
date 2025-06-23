7a2245cdf277a3e7d952c2f991ec84e83ad9b0d8
Mazo Vasiliy <mazo@protonmail.ch>
2025-06-23 08:55:00 +0300

    timer script text size fixed

--- scripts/.local/bin/dzen-timer.sh
+++ scripts/.local/bin/dzen-timer.sh
@@ -5,35 +5,58 @@ if [ "$1" == "--help" -o "$1" == "" ]; then
     exit 1
 fi
 
-# font
-font="Terminus:size=16:style=medium"
+# check for required tools
+command -v dzen2 > /dev/null || exit 1
+magick=$(command -v magick || command -v convert) || exit 1
 
-# background
-bg="#432d59"
-
-# foreground
-fg="#00ff00"
-
-# time output format
-format="%H:%M:%S"
-
-# current OS
-uname=$(uname)
-
-# message
-message="Time is up!"
+get_resource() {
+    name="$1"
+    xrdb -query | sed -r -n "/^$name:/ s/$name:[\\s\\t]+// p"
+}
 
 get_time() {
-    if test "$uname" = "FreeBSD"; then
+    format="%H:%M:%S"
+
+    if test "$(uname)" = "FreeBSD"; then
         date -u -j -f "%s" $1 +"$format"
     else
         date -u --date="@$1" +"$format"
     fi
 }
 
-mh=32
-mw1=$(xftwidth "$font" $(get_time 0))
-mw2=$(xftwidth "$font" "$message")
+# get settings from resources
+font=$(get_resource "bspwm-message[.]font")
+bg=$(get_resource "bspwm-message[.]background")
+fg=$(get_resource "bspwm-message[.]foreground")
+
+# default settings
+: "${font:='Terminus:size=16'}"
+: "${bg:='#432d59'}"
+: "${fg:='#00ff00'}"
+
+# get font file and size for magick
+fontfile=$(fc-match -f "%{file}" "$font")
+fontsize=$(fc-match -f "%{size}" "$font")
+
+# message
+message="Time is up!"
+
+# get screen dpi
+dpi=$(xdpyinfo | sed -n -r '/^[ ]*resolution:[ ]+[0-9]+x[0-9]+ dots/ s/^[ ]*resolution:[ ]+([0-9]+)x([0-9]+) dots .*/(\1+\2)\/2/p' | bc)
+
+# time message size
+size=$("$magick" -density $dpi -font "$fontfile" -pointsize $fontsize label:$(get_time 0) -format "%wx%h" info:)
+
+mw1=$(echo $size | cut -d 'x' -f 1)
+mh1=$(echo $size | cut -d 'x' -f 2)
+
+# timeout message size
+size=$("$magick" -density $dpi -font "$fontfile" -pointsize $fontsize label:"Time is up!" -format "%wx%h" info:)
+
+mw2=$(echo $size | cut -d 'x' -f 1)
+mh2=$(echo $size | cut -d 'x' -f 2)
+
+# position
 mx=0
 my=0
 
@@ -64,14 +87,14 @@ while true; do
 
     sleep 0.1
     timestamp=`date +%s`
-done | dzen2 -bg "$bg" -fg "$fg" -fn "$font" -w $mw1 -h $mh -x $mx -y $my
+done | dzen2 -bg "$bg" -fg "$fg" -fn "$font" -w $mw1 -h $mh1 -x $mx -y $my
 
-echo "$message" | dzen2 -p 5 -bg "$bg" -fg "$fg" -fn "$font" -w $mw2 -h $mh -x $mx -y $my &
+echo "$message" | dzen2 -p 5 -bg "$bg" -fg "$fg" -fn "$font" -w $mw2 -h $mh2 -x $mx -y $my &
 
-if which "notify-send"; then
+if which -s "notify-send"; then
     notify-send "$message"
 fi
 
 if test -f "$alarm"; then
-    mpv "$alarm"
+    mpv --terminal=no "$alarm"
 fi
