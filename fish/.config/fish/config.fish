03918ea16672a5d04567b3d1e68d40d9ecfcbfab
Mazo Vasiliy <mazo@protonmail.ch>
2025-07-18 23:31:09 +0300

    changed nnn config variables, added alias for lazygit

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,11 @@
 fish_add_path $HOME/.local/bin $HOME/.cargo/bin
 
-setenv EDITOR nvim
-setenv NVIM_GTK_NO_HEADERBAR 1
-setenv NNN_PLUG ";:fzplug;z:autojump;p:preview-tui;t:preview-tabbed"
-setenv NNN_OPTS "aAcdu"
-setenv NNN_FIFO "/tmp/nnn.fifo"
+setenv EDITOR (command -v nvim)
+
 setenv FZF_DEFAULT_OPTS "--no-mouse --color=bw --bind=ctrl-j:accept,ctrl-k:kill-line"
+
 setenv JQ_COLORS "0;39:0;39:0;39:0;39:0;32:1;39:1;39"
+
 setenv GOPATH $HOME/.gocode
 
 setenv XDG_DATA_HOME   $HOME/.local/share
@@ -21,24 +20,40 @@ set -g fish_greeting ""
 
 bind ctrl-z "jobs > /dev/null && echo && fg"
 
-alias cal "cal -M"
-alias parallel "parallel --will-cite"
-alias fc-select "fc-list | sed -r 's/^[^:]+:[ ]+([^,:]+)([,:].*)?/\1/' | sort -u | fzf --layout reverse-list"
-alias jq-record "jq '[.d, .s] | transpose | map({(.[1].n): .[0]}) | add'"
-alias jq-recordset "jq '[.d, [.s]] | combinations | transpose | map({(.[1].n): .[0]}) | add'"
-alias xml-beautify 'xmllint --format --encode utf-8 -'
-
-alias theme-toggle 'config.clj toggle ~/.config/gtk-3.0/settings.ini Settings gtk-theme-name Adwaita Adwaita-dark'
-alias theme-light  'config.clj set    ~/.config/gtk-3.0/settings.ini Settings gtk-theme-name Adwaita'
-alias theme-dark   'config.clj set    ~/.config/gtk-3.0/settings.ini Settings gtk-theme-name Adwaita-dark'
-
-set nnn (which nnn)
-if test -n "$nnn"
-    alias nnn "LANG=ru_RU.UTF-8 PAGER='nvimpager -p' NO_COLOR=1 $nnn"
+if test (uname) = "FreeBSD"
+    alias cal "cal -M"
+else
+    alias cal "cal -m"
 end
+
+alias parallel     "parallel --will-cite"
+alias fc-select    "fc-list | sed -r 's/^[^:]+:[ ]+([^,:]+)([,:].*)?/\1/' | sort -u | fzf --layout reverse-list"
+alias jq-record    "jq '[.d, .s] | transpose | map({(.[1].n): .[0]}) | add'"
+alias jq-recordset "jq '[.d, [.s]] | combinations | transpose | map({(.[1].n): .[0]}) | add'"
+alias xml-beautify "xmllint --format --encode utf-8 -"
+
+alias theme-toggle "config.clj toggle ~/.config/gtk-3.0/settings.ini Settings gtk-theme-name Adwaita Adwaita-dark"
+alias theme-light  "config.clj set    ~/.config/gtk-3.0/settings.ini Settings gtk-theme-name Adwaita"
+alias theme-dark   "config.clj set    ~/.config/gtk-3.0/settings.ini Settings gtk-theme-name Adwaita-dark"
+
+# Let's set some variables for nnn.
+set cmd (command -v nnn)
+if test -n "$cmd"
+    setenv NNN_PLUG ";:fzplug;z:autojump;p:preview-tui;t:preview-tabbed;d:dragdrop;c:rsynccp;e:suedit"
+    setenv NNN_OPTS "aAcdu"
+    setenv NNN_FIFO "/tmp/nnn.fifo"
+
+    alias nnn "LANG=ru_RU.UTF-8 PAGER='nvimpager -p' NO_COLOR=1 $cmd"
+end
+
+# Lazygit requires basename to be set in $EDITOR variable.
+set cmd (command -v lazygit)
+if test -n "$cmd"
+    alias lazygit "env EDITOR="(basename $EDITOR)" $cmd"
+end
+
 alias oil "nvim -c 'Oil'"
 alias nrepl "clj -Sdeps '{:deps {nrepl/nrepl {:mvn/version \"1.3.0\"} cider/cider-nrepl {:mvn/version \"0.50.3\"}}}' -M -m nrepl.cmdline --interactive"
-
 
 set host_config (status dirname)"$fold_config/config.fish#"(hostname | sed 's/\..*//')
 
