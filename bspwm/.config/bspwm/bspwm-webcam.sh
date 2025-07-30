8f43a7e9a695ef6fe6a8d452bfb606a6d8802876
Mazo Vasiliy <mazo@protonmail.ch>
2025-07-30 09:59:06 +0300

    changed way to get window id

--- bspwm/.config/bspwm/bspwm-webcam.sh
+++ bspwm/.config/bspwm/bspwm-webcam.sh
@@ -2,40 +2,38 @@
 
 TITLE=webcam
 
-window_id=$(bspc query -N          \
-    | xargs xtitle -f "%u %s\n"    \
-    | grep -E '^[0-9]+[ ]+webcam$' \
-    | sed -E 's/[ ]+.*//')
+window_id=$(xdotool search -name "$TITLE" | head -n 1)
 
 if test -n "$window_id"; then
     bspc node "$window_id" --flag hidden
-else
-    # screen width
-    sw=$(xwininfo -root | grep Width  | cut -d ':' -f 2)
-    sh=$(xwininfo -root | grep Height | cut -d ':' -f 2)
-
-    # webcam width
-    w=320
-    h=240
-
-    # gaps
-    gapx=6
-    gapy=6
-
-    x=$((sw-w-gapx))
-    y=$((sh-h-gapy))
-
-    bspc rule -a 'ffplay' -o            \
-        state=floating                  \
-        sticky=on                       \
-        center=off                      \
-        layer=above                     \
-        rectangle="${w}x${h}+${x}+${y}" \
-    && exec ffplay                      \
-        -f v4l2                         \
-        -window_title "$TITLE"          \
-        -video_size "${w}x${h}"         \
-        -framerate 25                   \
-        -i /dev/video0                  \
-        -fflags nobuffer
+    exit 0
 fi
+
+# screen width
+sw=$(xwininfo -root | grep Width  | cut -d ':' -f 2)
+sh=$(xwininfo -root | grep Height | cut -d ':' -f 2)
+
+# webcam width
+w=320
+h=240
+
+# gaps
+gapx=6
+gapy=6
+
+x=$((sw-w-gapx))
+y=$((sh-h-gapy))
+
+bspc rule -a 'ffplay' -o            \
+    state=floating                  \
+    sticky=on                       \
+    center=off                      \
+    layer=above                     \
+    rectangle="${w}x${h}+${x}+${y}" \
+&& exec ffplay                      \
+    -f v4l2                         \
+    -window_title "$TITLE"          \
+    -video_size "${w}x${h}"         \
+    -framerate 25                   \
+    -i /dev/video0                  \
+    -fflags nobuffer
