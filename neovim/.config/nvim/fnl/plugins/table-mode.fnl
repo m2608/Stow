0b20724262a7899ea28df2878c8845c31e645ff3
Mazo Vasiliy <mazo@protonmail.ch>
2025-08-04 00:14:46 +0300

    table-mode now uses markdown table format

--- neovim/.config/nvim/fnl/plugins/table-mode.fnl
+++ neovim/.config/nvim/fnl/plugins/table-mode.fnl
@@ -7,4 +7,11 @@
        "TableModeRealign"
        "TableModeToggle"
        "TableSort"
-       "Tableize"]}
+       "Tableize"]
+:init (fn []
+        (let [options [["table_mode_corner" "|"]
+                       ["table_mode_header_fillchar" "-"]]]
+          (each [_ option (ipairs options)]
+            (let [name (. option 1)
+                  value (. option 2)]
+              (tset vim.g name value)))))}
--- neovim/.config/nvim/lua/plugins/table-mode.lua
+++ neovim/.config/nvim/lua/plugins/table-mode.lua
@@ -1,2 +1,11 @@
 -- [nfnl] fnl/plugins/table-mode.fnl
-return {"dhruvasagar/vim-table-mode", lazy = true, cmd = {"TableAddFormula", "TableEvalFormulaLine", "TableModeDisable", "TableModeEnable", "TableModeRealign", "TableModeToggle", "TableSort", "Tableize"}}
+local function _1_()
+  local options = {{"table_mode_corner", "|"}, {"table_mode_header_fillchar", "-"}}
+  for _, option in ipairs(options) do
+    local name = option[1]
+    local value = option[2]
+    vim.g[name] = value
+  end
+  return nil
+end
+return {"dhruvasagar/vim-table-mode", lazy = true, cmd = {"TableAddFormula", "TableEvalFormulaLine", "TableModeDisable", "TableModeEnable", "TableModeRealign", "TableModeToggle", "TableSort", "Tableize"}, init = _1_}
