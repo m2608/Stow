75e7329d3e1909b486d0b214f616fd72a766afdb
Mazo Vasiliy <mazo@protonmail.ch>
2025-08-17 14:55:04 +0300

    script to change alacritty theme (imported)

---
+++ scripts/.local/bin/alacritty-theme.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+config="$HOME/.config/alacritty/alacritty.toml"
+themes="$HOME/Themes/alacritty/colors"
+
+self=$(basename "$0")
+
+help='Sets theme for alacritty. Themes folder: '$themes'
+Usage: '$self' <theme>
+'
+
+test -n "$1" || { echo "$help" ; exit 1; }
+theme="${themes}/base16-$1.toml"
+
+test -f "$theme" || { printf "Theme file not found: %s\n" "$theme" ; exit 1; }
+
+cat "$config" \
+| tomq --pp -R '(.general.import[] | select(test("/Themes/"))) = $theme' -- --arg theme "$theme" \
+| sponge "$config"
