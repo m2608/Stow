1dd06e5e16ae7ee985813ea2bd43eb14007882ea
Mazo Vasiliy <mazo@protonmail.ch>
2025-09-25 21:51:26 +0300

    check for theme existence in alacritty script

--- scripts/.local/bin/alacritty-theme.sh
+++ scripts/.local/bin/alacritty-theme.sh
@@ -12,6 +12,8 @@ Usage: '$self' <theme>
 test -n "$1" || { echo "$help" ; exit 1; }
 theme="${themes}/base16-$1.toml"
 
+command -v tomq > /dev/null || { echo "tomq tool required"; exit 1; }
+
 test -f "$theme" || { printf "Theme file not found: %s\n" "$theme" ; exit 1; }
 
 cat "$config" \
