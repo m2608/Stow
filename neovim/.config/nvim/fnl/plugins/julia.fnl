ca2f989338900c742f6ebe5c0ca2c96d386d848a
Mazo Vasiliy <mazo@protonmail.ch>
2025-11-02 22:16:01 +0300

    don't need julia plugin anymore

--- neovim/.config/nvim/fnl/plugins/julia.fnl
+++
@@ -1,7 +0,0 @@
-(local core (require "nfnl.core"))
-
-{1 "JuliaEditorSupport/julia-vim"
- :config
- (fn []
-   ;; (core.assoc vim.g "latex_to_unicode_auto" 1)
-   (core.assoc vim.g "latex_to_unicode_file_types" ".*"))}
--- neovim/.config/nvim/lua/plugins/julia.lua
+++
@@ -1,6 +0,0 @@
--- [nfnl] fnl/plugins/julia.fnl
-local core = require("nfnl.core")
-local function _1_()
-  return core.assoc(vim.g, "latex_to_unicode_file_types", ".*")
-end
-return {"JuliaEditorSupport/julia-vim", config = _1_}
