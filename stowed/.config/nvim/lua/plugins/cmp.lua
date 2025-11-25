5b4900132eb30fb845a80c97dff9712442768045
Mazo Vasiliy <mazo@protonmail.ch>
2025-11-26 00:09:58 +0300

    disable autocompletion in nvim

--- stowed/.config/nvim/lua/plugins/cmp.lua
+++ stowed/.config/nvim/lua/plugins/cmp.lua
@@ -13,6 +13,6 @@ local function _1_()
       return fallback()
     end
   end
-  return cmp.setup({sources = cmp.config.sources(core.map(_2_, {"conjure", "nvim_lsp", "buffer", "latex_symbols"})), mapping = {["<C-n>"] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}), ["<C-p>"] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}), ["<CR>"] = cmp.mapping.confirm({select = true}), ["<Space>"] = cmp.mapping(_3_, {"i", "s"})}})
+  return cmp.setup({sources = cmp.config.sources(core.map(_2_, {"conjure", "nvim_lsp", "buffer", "latex_symbols"})), completion = {autocomplete = false}, mapping = {["<C-n>"] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select}), ["<C-p>"] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select}), ["<CR>"] = cmp.mapping.confirm({select = true}), ["<Space>"] = cmp.mapping(_3_, {"i", "s"})}})
 end
 return {"hrsh7th/nvim-cmp", dependencies = {{"hrsh7th/cmp-nvim-lsp", event = "LspAttach"}, {"PaterJason/cmp-conjure", ft = {"clojure", "edn"}, dependencies = {"Olical/conjure", lazy = true}}, {"kdheepak/cmp-latex-symbols"}}, lazy = true, event = {"InsertEnter"}, config = _1_}
