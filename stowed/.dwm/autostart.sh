7470940a6be96a7631ebe974a327116997896323
Mazo Vasiliy <mazo@protonmail.com>
2025-12-06 12:43:06 +0300

    fix for machines without 'hostname' tool

--- stowed/.dwm/autostart.sh
+++ stowed/.dwm/autostart.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-# Выбираем скрипт в зависимости от имени машины.
-script="$(realpath $(dirname $0))/$(basename $0)#$(hostname | cut -d '.' -f 1)"
+if command -v hostname > /dev/null; then
+    hostname=$(hostname)
+elif command -v hostnamectl > /dev/null; then
+    hostname=$(hostnamectl hostname)
+fi
 
-if test -f "$script"; then
-    exec "$script"
+hostname=$(echo "$hostname" | cut -d '.' -f 1)
+
+# Выбираем скрипт в зависимости от имени машины.
+if test -n "$hostname"; then
+    script="$(realpath $(dirname $0))/$(basename $0)#$hostname"
+
+    if test -f "$script"; then
+        exec "$script"
+    fi
 fi
