0dbd42736f3a9eb31924315c1f89c75d707bbe98
Mazo Vasiliy <mazo@protonmail.com>
2025-12-06 12:42:47 +0300

    dwmstatus changes to support different configurations

--- stowed/.local/bin/dwmstatus.sh
+++ stowed/.local/bin/dwmstatus.sh
@@ -15,8 +15,9 @@ battery_status()
             fi
             ;;
         "Linux")
-            batt=`cat /sys/class/power_supply/BAT0/capacity`
-            stat=`cat /sys/class/power_supply/BAT0/status`
+            path=`find "/sys/class/power_supply" -name 'BAT*'`
+            batt=`cat "$path/capacity"`
+            stat=`cat "$path/status"`
             ;;
     esac
 
@@ -33,15 +34,25 @@ battery_status()
 volume_status()
 {
     icon="󰕿"
+    volume=""
 
-    volume=`sndioctl | sed '/^output.level=/!d;s/^output.level=//'`
-    volume=`echo "($volume*100)/1" | bc`
+    if command -v sndioctl > /dev/null; then
+        volume=`sndioctl | sed '/^output.level=/!d;s/^output.level=//'`
+        volume=`echo "($volume*100)/1" | bc`
+    elif command -v wpctl > /dev/null; then
+        volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d ' ' -f 2)
+        int_part=`echo "$volume" | cut -d '.' -f 1`
+        frc_part=`echo "$volume" | cut -d '.' -f 2`
+        volume=$((int_part*100 + frc_part))
+    fi
 
-    echo "$icon $volume"
+    test -n "$volume" && echo "$icon $volume♪"
 }
 
 temperature()
 {
+    icon=""
+    temp=""
     case "$OS" in 
         "FreeBSD")
             temp=`sysctl hw.acpi.thermal.tz0.temperature \
@@ -49,25 +60,34 @@ temperature()
             ;;
 
         "Linux")
-            temp=$(cat /sys/devices/platform/coretemp.0/hwmon/*/temp1_input | head -n 1)
-            temp=$((temp/1000))
+            path="/sys/devices/platform/coretemp.0"
+            if test -d "$path"; then
+                temp=$(cat "$path/hwmon/*/temp1_input" | head -n 1)
+                temp=$((temp/1000))
+            fi
             ;;
     esac
 
-    echo "$temp"
+    test -n "$temp" && echo "$icon $temp°C"
 }
 
 update_status()
 {
-    time=`date +"%d.%m %H:%M"`
+    time=$(date +"%d.%m %H:%M")
 
-    lang=`xkb-switch | tr 'a-z' 'A-Z'`
+    command -v xkb-switch && lang=$(xkb-switch | tr 'a-z' 'A-Z')
 
     batt=$(battery_status)
     temp=$(temperature)
     volume=$(volume_status)
 
-    xsetroot -name " $temp°C $batt $volume♪ $lang $time"
+    status=$(echo "${temp} ${batt} ${volume} ${lang} ${time}" | sed -r 's/[ ]+/ /g' | sed -r 's/^[ ]*//')
+
+    if command -v xsetroot > /dev/null; then
+        xsetroot -name "$status"
+    elif command -v dwmstatus-set > /dev/null; then
+        dwmstatus-set "$status"
+    fi
 }
 
 my_pid=$$
