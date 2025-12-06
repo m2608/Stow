6dc19f236328b6378d433536557aba0a75386fae
Mazo Vasiliy <mazo@protonmail.com>
2025-12-06 20:49:42 +0300

    makefile to fetch fonts

--- makefiles/fonts.mk
+++ makefiles/fonts.mk
@@ -1,27 +1,31 @@
-fetch-font-iosevka:
+CURRENT_FOLDER := $(shell dirname $(lastword $(MAKEFILE_LIST)))
+
+include $(CURRENT_FOLDER)/functions.mk
+
+fetch-iosevka:
 	$(call get-github-url,be5invis/Iosevka,"PkgTTF-Iosevka.*[.]zip$$") \
 		| xargs -n 1 curl -L -O --fail
 
-fetch-font-pragmasevka:
+fetch-pragmasevka:
 	$(call get-github-url,shytikov/pragmasevka,"^Pragmasevka_NF[.]zip$$") \
 		| xargs -n 1 curl -L -O --fail
 
-fetch-font-ioskeley:
+fetch-ioskeley:
 	$(call get-github-url,ahatem/IoskeleyMono,"^IoskeleyMono-TTF-Hinted.zip$$") \
 		| xargs -n 1 curl -L -O --fail
 
-fetch-font-victor:
+fetch-victor:
 	curl -L -O --fail "https://rubjo.github.io/victor-mono/VictorMonoAll.zip"
 
-fetch-font-sudo:
+fetch-sudo:
 	$(call get-github-url,jenskutilek/sudo-font,"^sudo[.]zip$$") \
 		| xargs -n 1 curl -L -O --fail
 
-fetch-font-departure:
+fetch-departure:
 	$(call get-github-url,rektdeckard/departure-mono,"^DepartureMono.*[.]zip$$") \
 		| xargs -n 1 curl -L -O --fail
 
-fetch-font-apl386:
+fetch-apl386:
 	curl -L -O https://abrudz.github.io/APL386/APL386.ttf
 
-all: fetch-font-iosevka fetch-font-pragmasevka fetch-font-ioskeley fetch-font-victor fetch-font-sudo fetch-font-departure
+all: fetch-iosevka fetch-pragmasevka fetch-ioskeley fetch-victor fetch-sudo fetch-departure fetch-apl386
