e1881923948c898c54d1f78c7a593dba8c08891f
Mazo Vasiliy <mazo@protonmail.com>
2026-01-29 13:25:59 +0300

    switched to bsdtar

--- makefiles/fonts.mk
+++ makefiles/fonts.mk
@@ -7,27 +7,32 @@ all: iosevka pragmasevka ioskeley victor sudo departure apl386
 
 iosevka:
 	$(call get-github-url,be5invis/Iosevka,"PkgTTF-Iosevka(Term|Aile|Etoile|Slab)?-[0-9.]+[.]zip$$") \
-		| xargs -I{} sh -c 'curl -s -L --fail {} | unzip -d "$(OUTPUT_FOLDER)" -- - "*.ttf"'
+		| xargs -n 1 curl -s -L --fail \
+		| bsdtar -C $(OUTPUT_FOLDER) -xf - -s '|.*/||' '*.ttf' 
 
 pragmasevka:
 	$(call get-github-url,shytikov/pragmasevka,"^Pragmasevka_NF[.]zip$$") \
-		| xargs -n 1 curl -s -L --fail | unzip -d "$(OUTPUT_FOLDER)" -- - "*.ttf"
+		| xargs -n 1 curl -s -L --fail \
+		| bsdtar -C $(OUTPUT_FOLDER) -xf - -s '|.*/||' '*.ttf' 
 
 ioskeley:
 	$(call get-github-url,ahatem/IoskeleyMono,"^IoskeleyMono-TTF-Hinted.zip$$") \
-		| xargs -n 1 curl -s -L --fail | unzip -j -d "$(OUTPUT_FOLDER)" -- - "*.ttf"
+		| xargs -n 1 curl -s -L --fail \
+		| bsdtar -C $(OUTPUT_FOLDER) -xf - -s '|.*/||' '*.ttf' 
 
 victor:
 	curl -s -L --fail "https://rubjo.github.io/victor-mono/VictorMonoAll.zip" \
-		| unzip -j -d "$(OUTPUT_FOLDER)" -- - "*.ttf"
+		| bsdtar -C $(OUTPUT_FOLDER) -xf - -s '|.*/||' '*.ttf' 
 
 sudo:
 	$(call get-github-url,jenskutilek/sudo-font,"^sudo[.]zip$$") \
-		| xargs -n 1 curl -s -L --fail | unzip -j -d "$(OUTPUT_FOLDER)" -- - "*.ttf"
+		| xargs -n 1 curl -s -L --fail \
+		| bsdtar -C $(OUTPUT_FOLDER) -xf - -s '|.*/||' '*.ttf' 
 
 departure:
 	$(call get-github-url,rektdeckard/departure-mono,"^DepartureMono.*[.]zip$$") \
-		| xargs -n 1 curl -s -L --fail | unzip -d "$(OUTPUT_FOLDER)" -- - "*.ttf"
+		| xargs -n 1 curl -s -L --fail \
+		| bsdtar -C $(OUTPUT_FOLDER) -xf - -s '|.*/||' '*.otf' 
 
 apl386:
-	curl -L -O --output-dir "$(OUTPUT_FOLDER)" https://abrudz.github.io/APL386/APL386.ttf
+	curl -L -O --output-dir $(OUTPUT_FOLDER) https://abrudz.github.io/APL386/APL386.ttf
