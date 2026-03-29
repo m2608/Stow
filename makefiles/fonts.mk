cac94dad751d1c7cd4ef3242d7b77c5a4614473d
Mazo Vasiliy <mazo@protonmail.ch>
2026-03-29 19:28:57 +0300

    Installation commands for more fonts added.

--- makefiles/fonts.mk
+++ makefiles/fonts.mk
@@ -6,9 +6,8 @@ OUTPUT_FOLDER := "$(HOME)/.fonts"
 all: iosevka pragmasevka ioskeley victor sudo departure apl386
 
 iosevka:
-	$(call get-github-url,be5invis/Iosevka,"PkgTTF-Iosevka(Term|Aile|Etoile|Slab)?-[0-9.]+[.]zip$$") \
-		| xargs -n 1 curl -s -L --fail \
-		| bsdtar -C $(OUTPUT_FOLDER) -xf - -s '|.*/||' '*.ttf'
+	$(call get-github-url,be5invis/Iosevka,"^PkgTTF-Iosevka(Term(SS[0-9]+)?|SS[0-9]+|Aile|Etoile|Slab)?-[0-9.]+[.]zip$$") \
+		| xargs -I {} sh -c "curl -L --fail {} | bsdtar -C $(OUTPUT_FOLDER) -xf - -s '|.*/||' '*.ttf'"
 
 pragmasevka:
 	$(call get-github-url,shytikov/pragmasevka,"^Pragmasevka_NF[.]zip$$") \
@@ -49,3 +48,17 @@ jetbrains:
 		| sed -n 1p \
 		| xargs -n 1 curl -L \
 		| bsdtar -C $(OUTPUT_FOLDER) -xf - -s '|.*/||' '*.ttf'
+
+bqn:
+	curl -L -O --output-dir $(OUTPUT_FOLDER) \
+		https://github.com/dzaima/BQN386/raw/refs/heads/master/BQN386.ttf
+
+nrk:
+	$(call get-github-url,N-R-K/NRK-Mono,"^NRK-Mono-v[0-9.]+[.]tar[.]zst$$") \
+		| xargs -n 1 curl -s -L --fail \
+		| tar -C $(OUTPUT_FOLDER) --zstd --strip-components 1 -xf - '*.ttf'
+
+iosvmata:
+	$(call get-github-url,N-R-K/Iosvmata,"^Iosvmata-v[0-9.]+[.]tar[.]zst$$") \
+		| xargs -n 1 curl -s -L --fail \
+		| tar -C $(OUTPUT_FOLDER) --zstd --strip-components 2 -xf - '*.ttf'
