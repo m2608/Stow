7a74d8aa10a5aa47a6289869cb4192d7e4b6bcc8
Mazo Vasiliy <mazo@protonmail.ch>
2026-04-27 13:27:20 +0300

    make alacritty to correctly apply config auto it has been changed

--- stowed/.local/bin/alacritty-theme.sh
+++ stowed/.local/bin/alacritty-theme.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-config="$HOME/.config/alacritty/alacritty.toml"
+# Определяем путь к файлу (для случаев, когда используется символическая ссылка).
+config=$(realpath "$HOME/.config/alacritty/alacritty.toml")
+
 themes="$HOME/Themes/output/base16-alacritty/colors"
 
 self=$(basename "$0")
@@ -16,7 +18,11 @@ command -v tomq > /dev/null || { echo "tomq tool required"; exit 1; }
 
 test -f "$theme" || { printf "Theme file not found: %s\n" "$theme" ; exit 1; }
 
-tomq --input-files "$config" "$theme" \
+data=$(tomq --input-files "$config" "$theme" \
     | jq -s '.[0] * .[1]' \
-    | tomq --pp -T \
-    | sponge "$config"
+    | tomq --pp -T)
+
+# Если файл не удалить, то при перенаправлении вывода данные будут записаны в файл
+# с тем же inode. В таких случаях alacritty почему-то не отслеживает изменение конфига.
+rm "$config"
+echo "$data" > "$config"
