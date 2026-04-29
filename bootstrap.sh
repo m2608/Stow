7d92153ef1d7d2ccfc0a9938f9eef793fc8b4522
Mazo Vasiliy <mazo@protonmail.ch>
2026-04-29 16:06:44 +0300

    stow added to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,20 +6,19 @@ pkg=""
 folder=$(dirname "$0")
 os=$(cat /etc/os-release | sed -n -r 's/^NAME="(.*)"/\1/p')
 
-packages="curl gcc git"
+packages="curl gcc git stow"
 make="make"
 build_janet=0
-build_jpm=0
 
 case $os in
     FreeBSD)
-        packages="$packages gmake janet jpm"
+        packages="$packages gmake janet"
         pkg_update="pkg update"
         pkg_install="pkg install -y"
         make="gmake"
         ;;
     Void*)
-        packages="$packages make janet jpm"
+        packages="$packages make janet"
         pkg_update="xbps-install -S"
         pkg_install="xbps-install -y"
         ;;
@@ -27,14 +26,12 @@ case $os in
         packages="$packages make build-base janet janet-dev"
         pkg_update="apk update"
         pkg_install="apk add"
-        build_jpm=1
         ;;
     Debian*)
         packages="$packages make"
         pkg_update="apt update"
         pkg_install="apt install -y"
         build_janet=1
-        build_jpm=1
         ;;
 esac
 
@@ -45,9 +42,3 @@ $pkg_install $packages
 if test $build_janet -ne 0; then
     "$make" -f "$folder/makefiles/janet.mk" PREFIX=/usr
 fi
-
-if test $build_jpm -ne 0;   then
-    "$make" -f "$folder/makefiles/jpm.mk"   PREFIX=/usr
-fi
-
-jpm install spork
