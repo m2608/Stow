ac93deebc658d6f3b73fa19e17aa9bf1287c0db5
Mazo Vasiliy <mazo@protonmail.com>
2026-06-26 09:03:48 +0300

    polybar now starts with host-dependent config

--- stowed/.config/polybar/polybar-restart.sh
+++ stowed/.config/polybar/polybar-restart.sh
@@ -3,6 +3,7 @@
 hostname=$({ hostname || hostnamectl hostname; } | cut -d "." -f 1)
 current_dir=$(dirname $0)
 
-pkill -x polybar
-sleep 0.5
+if pkill -x polybar; then
+    sleep 0.5
+fi
 exec polybar -c "$current_dir/config#$hostname.ini"
