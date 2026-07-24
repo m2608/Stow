a4d6e664e50441b829b75d57d70c9a8c706f8dc6
Mazo Vasiliy <mazo@protonmail.com>
2026-07-24 16:16:28 +0300

    vdi fish config

---
+++ stowed/.config/fish/config#vdi-dev-p6-160.fish
@@ -0,0 +1,39 @@
+setenv LC_COLLATE C
+
+set PATH \
+    $GOPATH/bin \
+    $HOME/.local/opt/go/bin/ \
+    $HOME/.cargo/bin \
+    $HOME/.local/bin \
+    /opt/Tensor/Saby \
+    /usr/local/bin \
+    /usr/local/sbin \
+    /usr/bin \
+    /usr/sbin \
+    /bin \
+    /sbin
+
+set os_name (cat /etc/os-release | sed -n '/^NAME=/p' | sed -r 's/^NAME="(.*)"$/\1/')
+if test "$os_name" = "Red Hat Enterprise Linux"
+    set PATH "$HOME/.local/bin.rh:$PATH"
+else if test "$os_name" = "Void"
+    set PATH "$HOME/.local/bin.void:$PATH"
+end
+
+setenv NNN_ARCHMNT ratarmount
+setenv VAULT_ADDR "https://csr-vault.sbis.ru"
+setenv PASSWORD_STORE_DIR "$HOME/Work/PasswordStore"
+
+alias less 'less -r'
+alias ls lsd
+# alias vimj 'vim -c "ru macros/justify.vim" -c "normal gqG" -c "normal gg" -c "normal _j" -c "set nonumber" -'
+alias mvi='mpv --config-dir=$HOME/.config/mvi'
+alias urldecode='python3 -c "import sys, urllib.parse as ul; print(ul.unquote_plus(sys.argv[1]))"'
+alias urlencode='python3 -c "import sys, urllib.parse as ul; print(ul.quote_plus(sys.argv[1]))"'
+alias ch "clickhouse local"
+# alias alacritty "MESA_GL_VERSION_OVERRIDE=3.3 $HOME/.cargo/bin/alacritty"
+# alias hiew 'wineconsole $HOME/.local/opt/hiew32demo/hiew32demo.exe'
+
+abbr hurl-proxy "hurl --proxy socks://127.0.0.1:5555"
+
+zoxide init fish | source
