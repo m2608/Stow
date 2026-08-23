8c396abee6ac139078056c72040e0a8371a348b3
Mazo Vasiliy <mazo@protonmail.com>
2026-08-23 23:10:44 +0300

    Переключение мониторов на steamdeck.

--- stowed/.config/bspwm/bspwm-desktop.sh
+++ stowed/.config/bspwm/bspwm-desktop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-test -n "$1" && test -n "$2" && test -n "$3" || exit 0
+test -n "$1" && test -n "$2" || exit 0
 
 # действие: focus, send
 action="$1"
@@ -8,23 +8,13 @@ action="$1"
 test "$action" = "focus" -o "$action" = "send" || exit 0
 
 # номер рабочего стола, на который переключаемся
-target_n="$2"
-
-# общее количество рабочих столов на одном мониторе
-total_n="$3"
-
-# номер текущего монитора
-mon_n=$(bspc query -M | sed -n "/$(bspc query -M -m focused)/{=;q;}")
-
-# номер десктопа в терминах bspwm
-desktop_n=$(((mon_n-1)*total_n+target_n))
+destination="$2"
 
 if test "$action" = "focus"; then
 
-    bspc desktop -f "$desktop_n"
-
-    $(dirname $0)/bspwm-message.sh "Workspace: $(bspc query -D -d focused --names)"
+    bspc desktop -f "$destination"
+    $(dirname $0)/bspwm-message.sh "Workspace: $destination_n"
 
 elif test "$action" = "send"; then
-    bspc node -d "$desktop_n"
+    bspc node -d "$destination"
 fi
--- stowed/.config/bspwm/bspwm-display.sh
+++ stowed/.config/bspwm/bspwm-display.sh
@@ -2,12 +2,51 @@
 
 hostname=$(hostnamectl hostname || hostname | cut -d '.' -f 1)
 
+# Функция переключает мониторы и переносит все окна со старого монитора,
+# сохраняя при этом рабочие столы.
+swap_monitors() {
+    src_mon="$1"
+    dst_mon="$2"
+
+    # Создаём рабочие столы на втором мониторе.
+    bspc monitor "$dst_mon" -d 6 7 8 9 10
+
+    # Выполняем для каждого рабочего стола.
+    for s in 1 2 3 4 5; do
+        d=$((s+5))
+        # Переносим ноды на новый монитор.
+        bspc query -N -d "$s" | xargs -I {} bspc node "{}" -d "$d"
+        # Удаляем старый рабочий стол.
+        bspc desktop "$s" -r
+        # Переименовываем новый рабочий стол.
+        bspc desktop "$d" -n "$s"
+    done
+    bspc monitor "$src_mon" -r
+}
+
 if test "$hostname" = "steamdeck"; then
     if xrandr --listmonitors | grep -q eDP; then
-        xrandr --output eDP --off
-        xrandr --output DisplayPort-0 --auto
+
+        src_mon="eDP"
+        dst_mon="DisplayPort-0"
+
+        xrandr \
+            --output "$dst_mon" --auto --primary \
+            --output "$src_mon" --off
+
+        swap_monitors "$src_mon" "$dst_mon"
     else
-        xrandr --output DisplayPort-0 --off
-        xrandr --output eDP --auto --rotate right
+        src_mon="DisplayPort-0"
+        dst_mon="eDP"
+
+        xrandr \
+            --output "$src_mon" --off \
+            --output "$dst_mon" --mode 800x1280 --rotate right --primary
+
+        swap_monitors "$src_mon" "$dst_mon"
+
     fi
+
+    bspc node @/ -B
 fi
+
